582a1d7284257c9ab73efba481088bcd62e2afa8
Wes Cummings <wes@a2-ai.com>
2024-12-16 21:44:26 +0000

    add comments

--- src/r_cmd.rs
+++ src/r_cmd.rs
@@ -5,7 +5,10 @@ trait R_CMD {
     fn r_libs_user(&self) -> &str;
     fn r_libs_site(&self) -> &str;
     fn pkg_path(&self) -> &str;
-    fn output_path(&self) -> Option<&str>;
+    // Using 1 output_dir arg for check and build, despite the outputs being very different. 
+    // `check` is not part of the standard work stream, so should be fine?
+    fn output_dir(&self) -> Option<&str>;
+    
 
     fn install(&self) {
         set_var("R_LIBS_USER", self.r_libs_user());
@@ -28,24 +31,23 @@ trait R_CMD {
             .arg("check")
             .arg(self.pkg_path())
             .arg("-o")
-            .arg(self.output_path().unwrap())
+            .arg(self.output_dir().unwrap())
             .output()
             .expect("TODO: R CMD check failed");
+        
         if !output.status.success() { eprintln!("R CMD check did not pass") }
     }
 
     fn build(&self) {
         set_var("R_LIBS_USER", self.r_libs_user());
         set_var("R_LIBS_SITE", self.r_libs_site());
-        let pkg_path = fs::canonicalize(self.pkg_path()).unwrap();
-        let output_path = fs::canonicalize(self.output_path().unwrap()).unwrap();
-        set_current_dir(self.output_path().unwrap()).unwrap();
+        let pkg_path = fs::canonicalize(self.pkg_path()).unwrap(); // setting to absolute path as setting current dir changes relative paths
+        set_current_dir(self.output_dir().unwrap()).unwrap(); // setting for built binary to be saved to specified dir
         let output = Command::new("R")
             .arg("CMD")
             .arg("INSTALL")
             .arg("--build")
             .arg(pkg_path)
-            .arg("-c")
             .output()
             .expect("TODO: R CMD INSTALL --build failed");
 
@@ -54,20 +56,21 @@ trait R_CMD {
 }
 
 mod test {
+    // tests were written for me to test as I worked, not as actual unit tests. Will clean before merging
     use super::*;
 
     struct TestStruct {
         r_libs_user: String,
         r_libs_site: String,
         path: String,
-        output_path: Option<String>,
+        output_dir: Option<String>,
     }
     
     impl R_CMD for TestStruct { 
         fn r_libs_site(&self) -> &str { &self.r_libs_site }
         fn r_libs_user(&self) -> &str { &self.r_libs_user }
         fn pkg_path(&self) -> &str { &self.path }
-        fn output_path(&self) -> Option<&str> { self.output_path.as_deref() }
+        fn output_dir(&self) -> Option<&str> { self.output_dir.as_deref() }
     }
 
     #[test]
@@ -76,7 +79,7 @@ mod test {
             r_libs_user: "/cluster-data/user-homes/wes/R/persieve".to_string(),
             r_libs_site: "/opt/R/4.4.1/lib/R/library".to_string(),
             path: "./src/tests/RCMD/R6_2.5.1.tar.gz".to_string(),
-            output_path: None,
+            output_dir: None,
         }.install();
     }
 
@@ -86,7 +89,7 @@ mod test {
             r_libs_user: "/cluster-data/user-homes/wes/R/persieve".to_string(),
             r_libs_site: "/opt/R/4.4.1/lib/R/library".to_string(),
             path: "./src/tests/RCMD/R6_2.5.1.tar.gz".to_string(),
-            output_path: Some("./src/tests/RCMD/R6.Rcheck".to_string()),
+            output_dir: Some("./src/tests/RCMD/".to_string()),
         }.check();
     }
 
@@ -96,7 +99,7 @@ mod test {
             r_libs_user: "/cluster-data/user-homes/wes/R/persieve".to_string(),
             r_libs_site: "/opt/R/4.4.1/lib/R/library".to_string(),
             path: "./src/tests/RCMD/R6_2.5.1.tar.gz".to_string(),
-            output_path: Some("./src/tests/RCMD".to_string()),
+            output_dir: Some("./src/tests/RCMD".to_string()),
         }.build();
     }
 }
