a2626b9578947e63f48e4043c0c4b4b15b24d8c7
Devin Pastoor <devin.pastoor@gmail.com>
2025-01-05 10:59:46 -0500

    conditionally output RV_LOG structured logs

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,10 @@ use clap::{Parser, Subcommand};
 use env_logger::Env;
 use rv::{
     cli::install::{execute_install, InstallArgs},
-    cli::plan::{execute_plan, PlanArgs, Distribution},
-    Config, 
+    cli::plan::{execute_plan, Distribution, PlanArgs},
+    Config,
 };
+use std::io::Write;
 use std::path::PathBuf;
 
 // use rand::Rng;
@@ -48,7 +49,17 @@ pub enum Command {
 
 fn try_main() {
     let cli = Cli::parse();
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    // if RV_LOG is set, then we want to use the RV_LOG setup
+    // otherwise we want to remove the leveled/structure components of the log and just output the args
+    if std::env::var("RV_LOG").is_ok() {
+        let env = Env::new().filter("RV_LOG");
+        env_logger::init_from_env(env);
+    } else {
+        env_logger::builder()
+            .parse_filters("info")
+            .format(|buf, record| writeln!(buf, "{}", record.args()))
+            .init();
+    }
     // TODO: parse config file here and fetch R version if needed
     // except for init
     // let config = Config::from_file(&cli.config_file);
