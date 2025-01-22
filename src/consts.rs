b285ad51783727bbf0b11a2ddb94849767c544e8
Wes Cummings <wes@a2-ai.com>
2025-01-22 21:20:23 +0000

    remove base_packages from consts

--- src/consts.rs
+++ src/consts.rs
@@ -19,20 +19,3 @@ pub const RECOMMENDED_PACKAGES: [&str; 15] = [
     "spatial", 
     "survival"
 ];
-// List obtained from the REPL: `rownames(installed.packages(priority="base"))`
-pub const BASE_PACKAGES: [&str; 14] = [
-    "base",
-    "compiler",
-    "datasets",
-    "grDevices",
-    "graphics",
-    "grid",
-    "methods",
-    "parallel",
-    "splines",
-    "stats",
-    "stats4",
-    "tcltk",
-    "tools",
-    "utils",
-];
