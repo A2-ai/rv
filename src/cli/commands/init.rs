4f812695d9b284e362953a3a12e38559de8626c0
Wes Cummings <wes@a2-ai.com>
2025-01-28 21:17:13 +0000

    simplify creation

--- src/cli/commands/init.rs
+++ src/cli/commands/init.rs
@@ -1,18 +1,20 @@
-use std::{fs::OpenOptions, io::{BufRead, BufReader, Write}, path::Path};
+use std::{
+    io::Write,
+    path::Path,
+};
 
-use fs_err::File;
-
-pub fn init<P: AsRef<Path>>(project_directory: P) -> Result<(), InitError>{
-    let project_directory = project_directory.as_ref().canonicalize().map_err(|e| InitError {
-        source: InitErrorKind::Io(e),
-    })?;
+/// This function initializes a given directory to be a rv project. It does this by:
+/// - Creating the directory if it does not exist
+/// - Creating the library directory (<path/to/directory>/rv/library)
+/// - Creating a .gitignore file within the rv subdirectory to ignore installed packages
+pub fn init<P: AsRef<Path>>(project_directory: P) -> Result<(), InitError> {
+    create_library_structure(&project_directory)?;
     write_gitignore(&project_directory)?;
-
     Ok(())
 }
 
 fn create_library_structure<P: AsRef<Path>>(project_directory: P) -> Result<(), InitError> {
-    std::fs::create_dir_all(project_directory.as_ref().join("rv").join("library")).map_err(
+    std::fs::create_dir_all(project_directory.as_ref().join("rv/library")).map_err(
         |e| InitError {
             source: InitErrorKind::Io(e),
         },
@@ -22,55 +24,19 @@ fn create_library_structure<P: AsRef<Path>>(project_directory: P) -> Result<(), 
 }
 
 fn write_gitignore<P: AsRef<Path>>(project_directory: P) -> Result<(), InitError> {
-    let project_directory = project_directory.as_ref();
-    if !project_directory.exists() {
-        create_library_structure(project_directory)?;
-    }
-
-    let file_path = project_directory.join("rv").join(".gitignore");
-    let ignored_paths = ["library/", "staging/"];
-
-    // check if gitignore file exists. If not, create one with "library/" and "staging/"
-    if !file_path.exists() {
-        let mut file = std::fs::File::create(&file_path)
-            .map_err(|e| InitError {
-                source: InitErrorKind::Io(e),
-            })?;
-
-        for p in ignored_paths {
-            file.write_all(p.as_bytes()).map_err(|e| InitError {
-                source: InitErrorKind::Io(e),
-            })?;
-        }
-
-        return Ok(())
+    let file_path = project_directory.as_ref().join("rv/.gitignore");
+    if file_path.as_path().exists() {
+        return Ok(());
     };
 
-    // if gitignore file does exist, write the missing "library/" and "staging" args
-    let file = File::open(&file_path).map_err(|e| InitError {
+    let mut file = std::fs::File::create(file_path).map_err(|e| InitError {
         source: InitErrorKind::Io(e),
     })?;
 
-    let reader = BufReader::new(file);
-    let mut lines = reader.lines().filter_map(Result::ok).collect::<Vec<_>>();
-
-    let mut file = OpenOptions::new()
-        .write(true)
-        .append(true)
-        .open(file_path)
-        .map_err(|e| InitError {
-            source: InitErrorKind::Io(e),
-        })?;
-
-    for p in ignored_paths {
-        if !lines.contains(&p.to_string()) {
-            file.write_all(p.as_bytes()).map_err(|e| InitError {
-                source: InitErrorKind::Io(e),
-            })?;
-            lines.push(p.to_string());
-        }
-    }
-
+    file.write_all(b"library/\nstaging/\n").map_err(|e| InitError {
+        source: InitErrorKind::Io(e)
+    })?;
+    
     Ok(())
 }
 
@@ -85,4 +51,18 @@ pub struct InitError {
 pub enum InitErrorKind {
     #[error(transparent)]
     Io(#[from] std::io::Error),
+}
+
+mod tests {
+    use std::fs;
+
+    use super::*;
+    #[test]
+    fn initialize_directory() {
+        let project_directory = "./test-init";
+        init(project_directory).unwrap();
+        assert!(Path::new(&format!("{project_directory}/rv/library")).exists());
+        assert!(Path::new(&format!("{project_directory}/rv/.gitignore")).exists());
+        fs::remove_dir_all(project_directory).unwrap();
+    }
 }
