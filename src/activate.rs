c7355ad58ba84752daa68d391fc4b21a3245a7e8
Wes Cummings <wes@a2-ai.com>
2025-02-19 16:07:04 +0000

    read file reconfigure

--- src/activate.rs
+++ src/activate.rs
@@ -1,14 +1,15 @@
 use std::{
-    fs::File,
-    io::{Read, Write},
+    io::Write,
     path::{Path, PathBuf},
 };
+
+use fs_err::{read_to_string, write, OpenOptions};
 
 use crate::consts::{GLOBAL_ACTIVATE_FILE_CONTENT, PROJECT_ACTIVATE_FILE_CONTENT};
 
 // constant file name and function to provide the R code string to source the file
 const ACTIVATE_FILE_NAME: &str = "rv/scripts/activate.R";
-fn source_activate_file_string() -> String {
+fn activation_string() -> String {
     format!(r#"source("{ACTIVATE_FILE_NAME}")"#)
 }
 
@@ -22,72 +23,50 @@ pub fn activate(dir: impl AsRef<Path>) -> Result<(), ActivateError> {
         });
     }
 
-    // read the .Rprofile, it the .Rprofile does not exist, create it. Then add the source command to the .Rprofile
-    let (rprofile_content, rprofile_file) = read_file(&dir.join(".Rprofile"))?;
-    if !rprofile_content.contains(&source_activate_file_string()) {
-        activate_rprofile(rprofile_content, rprofile_file)?;
+    let rprofile_path = dir.join(".Rprofile");
+    if !rprofile_path.exists() {
+        write(&rprofile_path, format!("{}\n", activation_string()))
+            .map_err(|e| ActivateError { source: ActivateErrorKind::Io(e) })?;
+    };
+
+    let content = read_to_string(&rprofile_path)
+        .map_err(|e| ActivateError{ source: ActivateErrorKind::Io(e) })?;
+
+    if content.contains(&activation_string()) {
+        return Ok(())
     }
 
-    write_activate_file(dir)?;
+    let mut file = OpenOptions::new().append(true).open(&rprofile_path)
+        .map_err(|e| ActivateError{ source: ActivateErrorKind::Io(e) })?;
 
+    writeln!(file, "{}", activation_string())
+        .map_err(|e| ActivateError{ source: ActivateErrorKind::Io(e) })?;
+
+    write_activate_file(dir)?;
+    
     Ok(())
 }
 
 pub fn deactivate(dir: impl AsRef<Path>) -> Result<(), ActivateError> {
     let dir = dir.as_ref();
-    let file_name = dir.join(".Rprofile");
+    let rprofile_path = dir.join(".Rprofile");
 
-    // if the .Rprofile does not exist, the directory is already "deactivated"
-    if !file_name.exists() {
+    if !rprofile_path.exists() {
         return Ok(());
     }
 
-    let (content, mut file) = read_file(file_name)?;
+    let content = read_to_string(&rprofile_path)
+        .map_err(|e| ActivateError { source: ActivateErrorKind::Io(e) })?;
 
-    // if the .Rprofile does not contain the activate source string, the directory is already "deactivated"
-    if !content.contains(&source_activate_file_string()) {
-        return Ok(());
-    }
+    let new_content = content
+        .lines()
+        .filter(|line| line != &activation_string())
+        .collect::<Vec<_>>()
+        .join("\n");
 
-    // remove the activate source string
-    let content = content.replace(&source_activate_file_string(), "");
-    file.write_all(content.as_bytes())
-        .map_err(|e| ActivateError {
-            source: ActivateErrorKind::Io(e),
-        })?;
+    write(&rprofile_path, new_content)
+        .map_err(|e| ActivateError { source: ActivateErrorKind::Io(e) })?;
 
-    Ok(())
-}
-
-// Read a file, if the file does not exist, create it
-// Return both the content and the File object for editing
-// Need to read the content, not just add new line for deactivate
-fn read_file(file_name: impl AsRef<Path>) -> Result<(String, File), ActivateError> {
-    let file_name = file_name.as_ref();
-    // if the file does not exist, create it and return the content as ""
-    if !file_name.exists() {
-        let file = File::create(file_name).map_err(|e| ActivateError {
-            source: ActivateErrorKind::Io(e),
-        })?;
-        return Ok((String::new(), file));
-    }
-    let mut file = File::open(file_name).map_err(|e| ActivateError {
-        source: ActivateErrorKind::Io(e),
-    })?;
-    let mut content = String::new();
-    file.read_to_string(&mut content)
-        .map_err(|e| ActivateError {
-            source: ActivateErrorKind::Io(e),
-        })?;
-    Ok((content, file))
-}
-
-fn activate_rprofile(rprofile_content: String, mut file: File) -> Result<(), ActivateError> {
-    let content = format!(r#"{}\n{}"#, rprofile_content, source_activate_file_string());
-    file.write_all(content.as_bytes())
-        .map_err(|e| ActivateError {
-            source: ActivateErrorKind::Io(e),
-        })?;
     Ok(())
 }
 
@@ -105,13 +84,14 @@ fn write_activate_file(dir: impl AsRef<Path>) -> Result<(), ActivateError> {
 
     // read the file and determine if the content within the activate file matches
     // File may exist but needs upgrade if file changes with rv upgrade
-    let (activate_content, mut file) = read_file(dir.join(ACTIVATE_FILE_NAME))?;
+    let activate_file_name = &dir.join(ACTIVATE_FILE_NAME);
+    let activate_content = read_to_string(activate_file_name).unwrap_or_default();
     if content == activate_content {
         return Ok(());
     }
 
     // Write the content of activate file
-    file.write_all(content.as_bytes())
+    write(activate_file_name, content)
         .map_err(|e| ActivateError {
             source: ActivateErrorKind::Io(e),
         })?;
