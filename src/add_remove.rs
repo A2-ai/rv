ddbdee32b8505825d53b3e0c9c62017e4e35e6e8
Wes Cummings <wes@a2-ai.com>
2025-02-19 18:25:30 +0000

    remove + reformatting + test

--- src/add_remove.rs
+++ src/add_remove.rs
@@ -1,9 +1,23 @@
 use std::path::Path;
 
 use std::fs;
+use fs_err::write;
 use toml_edit::{Array, DocumentMut, Formatted, Value};
 
-fn add(deps: Vec<String>, config_file: impl AsRef<Path>) -> Result<(), ConfigEditError> {
+pub fn add_dependencies(config_file: impl AsRef<Path>, deps: Vec<String>) -> Result<(), ConfigEditError> {
+    config_edit(config_file, deps, add_fxn)
+}
+
+pub fn remove_dependencies(config_file: impl AsRef<Path>, deps: Vec<String>) -> Result<(), ConfigEditError> {
+    config_edit(config_file, deps, remove_fxn)
+}
+
+// add and remove are almost completely the same except for the mutation of the dependency array. 
+// Thus one function calling different mutation functions instead of two functions calling many shared functions
+fn config_edit<F>(config_file: impl AsRef<Path>, deps: Vec<String>, edit_fxn: F) -> Result<(), ConfigEditError>
+where
+    F: Fn(&mut Array, Vec<String>),
+{
     let config_file = config_file.as_ref();
     let config_content = fs::read_to_string(&config_file).map_err(|e| ConfigEditError {
         path: config_file.into(),
@@ -37,35 +51,53 @@ fn add(deps: Vec<String>, config_file: impl AsRef<Path>) -> Result<(), ConfigEdi
         last.decor_mut().set_suffix("");
     }
 
-    let project_repo_dep_names = repository_dependencies(project_dependencies);
-    for d in deps {
-        if !project_repo_dep_names.contains(&d) {
-            project_dependencies.push(Value::String(Formatted::new(d)));
-            if let Some(last) = project_dependencies.iter_mut().last() {
-                last.decor_mut().set_prefix("\n    ");
-            }
-        }
-    }
+    edit_fxn(project_dependencies, deps);
+
+    project_dependencies.set_trailing("\n");
+    project_dependencies.set_trailing_comma(true);
+
+    write(config_file, doc.to_string())
+        .map_err(|e| ConfigEditError {
+            path: config_file.into(),
+            source: ConfigEditErrorKind::Io(e),
+        })?;
 
     Ok(())
 }
 
-fn repository_dependencies(arr: &Array) -> Vec<String> {
-    arr
-        .iter()
-        .filter_map(|v| match v {
-            Value::String(s) => Some(s.value().to_string()),
+fn add_fxn(config_deps: &mut Array, add_deps: Vec<String>) {
+    let config_dep_names = config_deps.iter().filter_map(|v| get_dependency_name(v)).collect::<Vec<_>>();
+    for d in add_deps {
+        if !config_dep_names.contains(&d) {
+            config_deps.push(Value::String(Formatted::new(d)));
+            // Couldn't format value before pushing, so adding formatting after its added
+            if let Some(last) = config_deps.iter_mut().last() {
+                last.decor_mut().set_prefix("\n    ");
+            }        
+        }
+    }
+}
+
+fn remove_fxn(config_deps: &mut Array, add_deps: Vec<String>) {
+    config_deps.retain(|v| {
+        if let Some(name) = get_dependency_name(v) {
+            !add_deps.contains(&name)
+        } else {
+            true
+        }
+    });
+}
+
+fn get_dependency_name(value: &Value) -> Option<String> {
+    match value {
+        Value::String(s) => Some(s.value().to_string()),
             Value::InlineTable(t) => {
-                let name = t.get("name");
-                if let Some(Value::String(s)) = name {
-                    Some(s.value().to_string())
-                } else {
-                    None
-                }
+                t.get("name")
+                    .and_then(|v| v.as_str())
+                    .map(|s| s.to_string())
             },
             _ => None
-        })
-        .collect::<Vec<_>>()
+    }
 }
 
 #[derive(Debug, thiserror::Error)]
@@ -86,12 +118,24 @@ pub enum ConfigEditErrorKind {
 }
 
 mod tests {
+    use std::path::Path;
+
+    use fs_err::{read_to_string, write};
+    use tempfile::tempdir;
+
     #[test]
     fn tester() {
-        super::add(
-            vec!["test_pkg".to_string()],
-            "example_projects/simple/rproject.toml",
-        )
-        .unwrap();
+        let content = read_to_string("src/tests/valid_config/all_fields.toml").unwrap();
+        let mut out = String::new();
+        let tmp_dir = tempdir().unwrap();
+        let config_file = tmp_dir.path().join("rproject.toml");
+        write(&config_file, content).unwrap();
+        super::add_dependencies(&config_file, vec!["test_pkg".to_string(), "dplyr".to_string()]).unwrap();
+        out.push_str("===ADDED==========");
+        out.push_str(&read_to_string(&config_file).unwrap());
+        super::remove_dependencies(&config_file, vec!["test_pkg".to_string(), "dplyr".to_string(), "renv".to_string()]).unwrap();
+        out.push_str("===REMOVED==========");
+        out.push_str(&read_to_string(&config_file).unwrap());
+        insta::assert_snapshot!("add_remove", out);
     }
 }
