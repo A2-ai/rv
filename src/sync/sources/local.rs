dc35609f3643e2ece26e1e41b8e2d9fb6d1715ff
Vincent Prouillet <github@vincentprouillet.com>
2025-03-03 12:52:18 +0100

    Make local deps relative to project dir + fix sync errors handling

--- src/sync/sources/local.rs
+++ src/sync/sources/local.rs
@@ -11,20 +11,22 @@ use crate::{is_binary_package, RCmd, ResolvedDependency};
 
 pub(crate) fn install_package(
     pkg: &ResolvedDependency,
+    project_dir: &Path,
     library_dir: &Path,
     r_cmd: &impl RCmd,
 ) -> Result<(), SyncError> {
     // First we check if the package exists in the library and what's the mtime in it
-    let local_path = Path::new(pkg.source.source_path()).canonicalize()?;
+    let local_path = Path::new(pkg.source.source_path());
     let tempdir = tempfile::tempdir()?;
+    let canon_path = fs::canonicalize(project_dir.join(local_path))?;
 
     // TODO: use the file sha somehow?
-    let actual_path = if local_path.is_file() {
+    let actual_path = if canon_path.is_file() {
         // TODO: we're already doing that in resolve, that's wasteful
-        let path = untar_archive(std::fs::read(&local_path)?.as_slice(), tempdir.path())?;
-        path.unwrap_or_else(|| local_path.clone())
+        let path = untar_archive(fs::read(&canon_path)?.as_slice(), tempdir.path())?;
+        path.unwrap_or_else(|| canon_path.clone())
     } else {
-        local_path.clone()
+        canon_path.clone()
     };
 
     if is_binary_package(&actual_path, pkg.name.as_ref()) {
@@ -34,7 +36,7 @@ pub(crate) fn install_package(
         );
         LinkMode::Copy.link_files(
             pkg.name.as_ref(),
-            actual_path,
+            &actual_path,
             library_dir.join(pkg.name.as_ref()),
         )?;
     } else {
@@ -43,8 +45,8 @@ pub(crate) fn install_package(
     }
 
     // If it's a dir, save the dir mtime
-    if local_path.is_dir() {
-        let local_mtime = mtime_recursive(&local_path)?;
+    if actual_path.is_dir() {
+        let local_mtime = mtime_recursive(&actual_path)?;
 
         // And just write the mtime in the output directory
         let mut file = fs::File::create(
