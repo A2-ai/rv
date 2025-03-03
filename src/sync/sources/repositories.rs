86d8b96b713d41ed136f0125f9e7c7572ef91e34
wes-a2ai <wes@a2-ai.com>
2025-03-03 20:21:17 +0000

    add additional fall back to archive

--- src/sync/sources/repositories.rs
+++ src/sync/sources/repositories.rs
@@ -7,10 +7,11 @@ use fs_err as fs;
 use crate::cache::InstallationStatus;
 use crate::http::Http;
 use crate::package::PackageType;
+use crate::r_cmd::InstallError;
 use crate::sync::errors::SyncError;
 use crate::sync::LinkMode;
 use crate::{
-    get_tarball_urls, is_binary_package, DiskCache, HttpDownload, RCmd, ResolvedDependency,
+    get_tarball_urls, is_binary_package, DiskCache, HttpDownload, PackagePaths, RCmd, ResolvedDependency
 };
 
 pub(crate) fn install_package(
@@ -21,7 +22,20 @@ pub(crate) fn install_package(
 ) -> Result<(), SyncError> {
     let pkg_paths =
         cache.get_package_paths(&pkg.source, Some(&pkg.name), Some(&pkg.version.original));
+    install(pkg, library_dir, cache, &pkg_paths, r_cmd)?;
+    // And then we always link the binary folder into the staging library
+    LinkMode::new().link_files(&pkg.name, &pkg_paths.binary, library_dir)?;
 
+    Ok(())
+}
+
+fn install(
+    pkg: &ResolvedDependency,
+    library_dir: &Path,
+    cache: &DiskCache,
+    pkg_paths: &PackagePaths,
+    r_cmd: &impl RCmd,
+) -> Result<(), SyncError> {
     let compile_package = || {
         let source_path = pkg_paths.source.join(pkg.name.as_ref());
         log::debug!("Compiling package from {}", source_path.display());
@@ -48,26 +62,10 @@ pub(crate) fn install_package(
                 get_tarball_urls(pkg, &cache.r_version, &cache.system_info);
             let http = Http {};
 
-            let download_and_install_source = || -> Result<(), SyncError> {
-                log::debug!(
-                    "Downloading package {} ({}) as source tarball",
-                    pkg.name,
-                    pkg.version.original
-                );
-                http.download_and_untar(&source_url, &pkg_paths.source, false)?;
-                compile_package()?;
-                Ok(())
-            };
-
-            if pkg.kind == PackageType::Source || binary_url.is_none() {
-                download_and_install_source()?;
-            } else {
+            if pkg.kind == PackageType::Binary && binary_url.is_some() {
                 // If we get an error doing the binary download, fall back to source
-                if let Err(e) =
-                    http.download_and_untar(&binary_url.clone().unwrap(), &pkg_paths.binary, false)
-                {
+                if let Err(e) = http.download_and_untar(&binary_url.clone().unwrap(), &pkg_paths.binary, false) {
                     log::warn!("Failed to download/untar binary package from {}: {e:?}, falling back to {source_url}", binary_url.clone().unwrap());
-                    download_and_install_source()?;
                 } else {
                     // Ok we download some tarball. We can't assume it's actually compiled though, it could be just
                     // source files. We have to check first whether what we have is actually binary content.
@@ -85,14 +83,23 @@ pub(crate) fn install_package(
                         }
                         compile_package()?;
                     }
+                    return Ok(());
                 }
             }
+
+            // Try to install source
+            if let Err(e) = http.download_and_untar(&source_url, &pkg_paths.source, false) {
+                log::warn!("Failed to download/untar source package from {source_url}: {e:?}, falling back to {archive_url}");
+            } else {
+                compile_package()?;
+                return Ok(());
+            }
+
+            // Try to install from archive
+            http.download_and_untar(&archive_url, &pkg_paths.source, false)?;
+            compile_package()?;
         }
         _ => {}
     }
-
-    // And then we always link the binary folder into the staging library
-    LinkMode::new().link_files(&pkg.name, &pkg_paths.binary, library_dir)?;
-
     Ok(())
 }
