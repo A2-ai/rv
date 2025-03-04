f5a394023c490f324ebc2c4a75a048f85f258f04
wes-a2ai <wes@a2-ai.com>
2025-03-04 12:50:49 -0500

    look for R.bat on path for windows

--- src/r_cmd.rs
+++ src/r_cmd.rs
@@ -70,6 +70,14 @@ pub fn find_r_version_command(r_version: &Version) -> Option<RCommandLine> {
         return Some(RCommandLine { r: None });
     }
 
+    // For windows, R installed/managed by rig is has the extension .bat
+    if cfg!(windows) {
+        if does_r_cmd_match_version(&RCommandLine {r: Some(PathBuf::from("R.bat"))}, r_version) {
+            log::debug!("R {r_version} found on the path from `rig`");
+            return Some(RCommandLine { r: Some(PathBuf::from("R.bat")) })
+        }
+    }
+
     let opt_r = PathBuf::from("/opt/R");
     if !opt_r.is_dir() {
         return None;
