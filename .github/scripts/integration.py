d7f44354269675da45f6193b24d23f9a2fbb780f
Vincent Prouillet <github@vincentprouillet.com>
2025-03-06 16:15:00 +0100

    Do not move staging to library in dry run (#149)
    Closes #148

--- .github/scripts/integration.py
+++ .github/scripts/integration.py
@@ -1,8 +1,22 @@
 import os
 import subprocess
 
-
 PARENT_FOLDER = "example_projects"
+
+def run_cmd(cmd, path):
+    print(f"Running {cmd}")
+    command = ["./target/release/rv", cmd, "--config-file", path, "-vvv"]
+    result = subprocess.run(command, capture_output=True, text=True)
+    print(result.stdout)
+    print(result.stderr)
+
+    # Check for errors
+    if result.returncode != 0:
+        print(f"Command failed with error: {result.stderr}")
+        exit(1)
+
+    return result.stdout
+
 
 def run_examples():
     items = os.listdir(PARENT_FOLDER)
@@ -13,14 +27,13 @@ def run_examples():
         subfolder_path = os.path.join(PARENT_FOLDER, subfolder, "rproject.toml")
         print(f"Processing example: {subfolder_path}")
 
-        command = ["./target/release/rv", "sync", "--config-file", subfolder_path, "-vvv"]
-        result = subprocess.run(command, capture_output=True, text=True)
-        print(result.stdout)
-        print(result.stderr)
+        run_cmd("sync", subfolder_path)
+        run_cmd("plan", subfolder_path)
+        library_path = run_cmd("library", subfolder_path)
+        folder_count = len(os.listdir(library_path.strip()))
 
-        # Check for errors
-        if result.returncode != 0:
-            print(f"Command failed with error: {result.stderr}")
+        if folder_count == 0:
+            print(f"No folders found in library for {subfolder}")
             return 1
 
     return 0
