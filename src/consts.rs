07da3c4c8a2530148c3c8ce526c85058486c1db3
Wes Cummings <wes@a2-ai.com>
2025-03-28 14:03:56 -0400

    remove extra comma from activate script (#184)
    * remove extra comma from activate script
    
    * move back to 0.3.3

--- src/consts.rs
+++ src/consts.rs
@@ -64,7 +64,7 @@ pub(crate) const ACTIVATE_FILE_TEMPLATE: &str = r#"local({%global wd content%
 		rv_r_ver <- sub("r-version: (.+)", "\\1", grep("^r-version:", rv_info, value = TRUE))
 		repo_str <- sub("repositories: ", "", grep("^repositories:", rv_info, value = TRUE))
 		repo_entries <- gsub("[()]", "", strsplit(repo_str, "), (", fixed = TRUE)[[1]])
-    repo_list <- trimws(sub(".*, ", "", repo_entries)),  # Extract URL
+    repo_list <- trimws(sub(".*, ", "", repo_entries))  # Extract URL
     names(repo_list) <- trimws(sub(", .*", "", repo_entries))   # Extract Name
 		# this might not yet exist, so we'll normalize it but not force it to exist
 		# and we create it below as needed
