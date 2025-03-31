04c93f5d2aaf24c41ec1cbe63762329b10e15793
Vincent Prouillet <github@vincentprouillet.com>
2025-03-31 14:28:21 +0200

    Control max num of cpu via env var

--- src/sync/handler.rs
+++ src/sync/handler.rs
@@ -7,6 +7,7 @@ use std::sync::atomic::{AtomicBool, AtomicUsize, Ordering};
 use std::sync::{Arc, Mutex};
 use std::time::Duration;
 
+use crate::consts::NUM_CPUS_ENV_VAR_NAME;
 use crate::lockfile::Source;
 use crate::package::PackageType;
 use crate::sync::changes::SyncChange;
@@ -33,6 +34,10 @@ impl<'a> SyncHandler<'a> {
         cache: &'a DiskCache,
         staging_path: impl AsRef<Path>,
     ) -> Self {
+        let max_workers = std::env::var(NUM_CPUS_ENV_VAR_NAME)
+            .ok()
+            .and_then(|x| x.parse::<usize>().ok())
+            .unwrap_or_else(num_cpus::get);
         Self {
             project_dir,
             library,
@@ -41,7 +46,7 @@ impl<'a> SyncHandler<'a> {
             dry_run: false,
             show_progress_bar: false,
             has_lockfile: false,
-            max_workers: num_cpus::get(),
+            max_workers,
         }
     }
 
