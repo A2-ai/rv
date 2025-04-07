f836c17137b100f849d34b023af93046f17d65c5
wes-a2ai <wes@a2-ai.com>
2025-04-07 14:52:39 +0000

    lockfile test

--- .github/scripts/e2e.py
+++ .github/scripts/e2e.py
@@ -1,4 +1,6 @@
 import os
+import re
+from pathlib import Path
 import subprocess
 import shutil
 
@@ -14,13 +16,14 @@ def run_rv_cmd(cmd, args):
         print(f"Command failed with error: {result.stderr}")
         exit(1)
 
-    return result.stdout
+    return result.stdout    
 
 def run_r_script(script):
     print(f">> Running R script: {script}")
     command = ["Rscript", "-e", script]
     result = subprocess.run(command, capture_output=True, text=True)
     
+    # Check for errors
     if result.returncode != 0:
         print(f"Command failed with error: {result.stderr}")
         exit(1)
@@ -40,6 +43,27 @@ def load_r_profile():
 
     return result.stdout
 
+def edit_repositories():
+    print(f">> Replacing repositories field with 'https://packagemanager.posit.co/cran/2025-04-07'")
+    file_path = Path("rproject.toml")
+    content = file_path.read_text()
+    pattern = r'repositories\s*=\s*\[.*?\]'
+    replace = 'repositories = [\n\t{alias = "RPSM", url = "https://packagemanager.posit.co/cran/2025-04-07"},\n]'
+    new_content = re.sub(pattern, replace, content, flags=re.DOTALL)
+    file_path.write_text(new_content)
+    
+def run_plan_for_failure():
+    # After edited to config to change repositories, verify plan results in error
+    command = ["rv", "plan", "-vvv"]
+    result = subprocess.run(command, capture_output=True, text=True)
+    
+    # Check for errors
+    if result.returncode == 0:
+        print(f"Packages resolved when not intended: {result.stdout}")
+        exit(1)
+        
+    return result.stderr
+
 def run_test():
     os.environ["PATH"] = os.path.abspath("./target/release/") + os.pathsep + os.environ.get("PATH", "")
     if os.path.exists(PROJ_PATH):
@@ -48,10 +72,6 @@ def run_test():
     run_rv_cmd("init", [PROJ_PATH])
     os.chdir(PROJ_PATH)
     load_r_profile()
-    # Print the content of rproject.toml
-    with open("rproject.toml", "r") as file:
-        print(">> Content of rproject.toml:")
-        print(file.read())
     
     # Add R6 and verify it loads (and .Rprofile loading sets .libPaths)
     run_rv_cmd("add", ["R6"])
@@ -71,6 +91,10 @@ def run_test():
     if "+ fansi" not in plan:
         print(f"Dry run add did not result in correct plan: {plan}")
         exit(1)
+        
+    # Edit the repositories field to test that lockfile is invalid if repo not present in config anymore
+    edit_repositories()
+    run_plan_for_failure()
         
     return 0
 
