bdf67408d892cb20856b8128584eec710237ebe3
wes-a2ai <wes@a2-ai.com>
2025-04-08 20:53:17 +0000

    upgrade test

--- .github/scripts/e2e.py
+++ .github/scripts/e2e.py
@@ -61,28 +61,29 @@ def init_test():
     finally:
         os.chdir(original_dir)
         
-# def upgrade_test():
-#     run_rv_cmd("upgrade", ["-c", os.path.join(UPGRADE_FOLDER, CONFIG_FILE)])
-#     library = run_rv_cmd("library", ["-c", os.path.join(UPGRADE_FOLDER, CONFIG_FILE)])
-#     pkg_version = run_r_script(f"packageVersion('ggplot2', lib.loc = '{library}')")
-#     if "3.5.1" not in pkg_version:
-#         print(f"Incorrect package version installed during upgrade: {pkg_version}")
-#         exit(1)    
-#     return pkg_version
+def upgrade_test():
+    config_path = os.path.join(UPGRADE_FOLDER, CONFIG_FILE)
+    res = run_rv_cmd("upgrade", ["-c", config_path])
+    library = run_rv_cmd("library", ["-c", os.path.join(UPGRADE_FOLDER, CONFIG_FILE)]).strip()
+    pkg_version = run_r_script(f"packageVersion('ggplot2', lib.loc = '{library}')")
+    if "3.5.1" not in pkg_version:
+        print(f"Incorrect package version installed during upgrade: {pkg_version}")
+        exit(1)    
+    return pkg_version
     
     
 def run_test():
     os.environ["PATH"] = os.path.abspath("./target/release/") + os.pathsep + os.environ.get("PATH", "")
     items = os.listdir(PARENT_FOLDER)
-    for subfolder in items:
-        dir = os.path.join(PARENT_FOLDER, subfolder)
-        config_path = os.path.join(dir,CONFIG_FILE)
-        print(f"===== Processing example: {config_path} ======")
-        stdout = run_rv_cmd("summary", ["-c", config_path], True) # should be False once fixes to these issues are in
-        print(f"{stdout}")
-        
-    init_test()
-    # upgrade_test()
+    # for subfolder in items:
+    #     dir = os.path.join(PARENT_FOLDER, subfolder)
+    #     config_path = os.path.join(dir,CONFIG_FILE)
+    #     print(f"===== Processing example: {config_path} ======")
+    #     stdout = run_rv_cmd("summary", ["-c", config_path], True) # should be False once fixes to these issues are in
+    #     print(f"{stdout}")
+    # 
+    # init_test()
+    upgrade_test()
     
 if __name__ == "__main__":
     exit(run_test())
