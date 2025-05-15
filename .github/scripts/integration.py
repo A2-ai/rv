badd6e62258971a1414e9dd5fee3772f7066d350
Wes Cummings <wes@a2-ai.com>
2025-05-15 17:08:00 -0400

    More tests (#228)
    * do not skip git plan check
    
    * test for init, activate, upgrade, summary, and rvr
    
    * e2e test runner
    
    * e2e runner
    
    * point to the release build of rv
    
    * add target/release to PATH for .Rprofile

--- .github/scripts/integration.py
+++ .github/scripts/integration.py
@@ -4,7 +4,7 @@ import subprocess
 import shutil
 
 PARENT_FOLDER = "example_projects"
-SKIP_PLAN_CHECK = ["git", "no-lockfile", "url", "custom-lib-path"]
+SKIP_PLAN_CHECK = ["no-lockfile", "url", "custom-lib-path"]
 
 def run_cmd(cmd, path, json = False):
     print(f">> Running rv {cmd}")
