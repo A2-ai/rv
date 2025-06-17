8c4c071b4e3deec9ab4beea413a6763639b35d24
Wes Cummings <wes@a2-ai.com>
2025-06-17 05:54:52 -0400

    Sys Reqs, no packages (#274)
    * fix 20.04 sys reqs issue
    
    * test

--- src/system_req.rs
+++ src/system_req.rs
@@ -46,6 +46,8 @@ impl SysDep {
 
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
 struct Requirements {
+    // not all requirements have packages. Some are pre_/post_install
+    #[serde(default)]
     packages: Vec<String>,
 }
 
@@ -169,4 +171,16 @@ pub fn check_installation_status(
     }
 
     out
+}
+
+#[cfg(test)]
+mod test {
+    use std::fs;
+    use super::Response;
+
+    #[test]
+    fn test_ubuntu_20_04() {
+        let content = fs::read_to_string("src/tests/sys_reqs/ubuntu_20.04.json").unwrap();
+        assert!(serde_json::from_str::<Response>(&content).is_ok());
+    }
 }
