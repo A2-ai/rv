1c4b620f0ce3c0279160de3a4b2922db301db7cb
Andriy Massimilla <andriy@a2-ai.com>
2025-08-20 08:54:04 -0400

    Fix returned R cmd path when resolving macOS rig version (#327)
    * Fix returned R cmd path when resolving macOS rig version
    
    * Add TODO, update cfg target conditional, build cmd object for early returns, standardize logs

--- justfile
+++ justfile
@@ -1,6 +1,9 @@
 run *args:
     cargo run --features=cli -- {{args}}
 
+build: 
+    cargo build --features=cli
+
 test:
     cargo test --features=cli
 
--- src/r_cmd.rs
+++ src/r_cmd.rs
@@ -104,11 +104,13 @@ pub struct RCommandLine {
 }
 
 pub fn find_r_version_command(r_version: &Version) -> Result<RCommandLine, VersionError> {
+    // TODO: increase test coverage for this function
+
     let mut found_r_vers = Vec::new();
     // Give preference to the R version on the path
     if let Ok(path_r) = (RCommandLine { r: None }).version() {
         if r_version.hazy_match(&path_r) {
-            log::debug!("R {r_version} found on the path");
+            log::debug!("R found on the path: {path_r}");
             return Ok(RCommandLine { r: None });
         }
         found_r_vers.push(path_r.original);
@@ -122,14 +124,18 @@ pub fn find_r_version_command(r_version: &Version) -> Result<RCommandLine, Versi
         if let Some(arch) = info.architecture() {
             let rig_r_bin_path =
                 PathBuf::from(format!("R-{}.{}-{}", major_minor[0], major_minor[1], arch));
-            if let Ok(path_rig_r) = (RCommandLine {
+
+            let rig_r_cmd = RCommandLine {
                 r: Some(rig_r_bin_path),
-            })
-            .version()
-            {
+            };
+
+            if let Ok(path_rig_r) = rig_r_cmd.version() {
                 if r_version.hazy_match(&path_rig_r) {
-                    log::debug!("R {r_version} found on the path via rig pattern");
-                    return Ok(RCommandLine { r: None });
+                    log::debug!(
+                        "R found on the path via rig pattern: {}",
+                        rig_r_cmd.clone().r.unwrap().display()
+                    );
+                    return Ok(rig_r_cmd);
                 }
                 found_r_vers.push(path_rig_r.original);
             }
@@ -137,17 +143,18 @@ pub fn find_r_version_command(r_version: &Version) -> Result<RCommandLine, Versi
     }
 
     // For windows, R installed/managed by rig is has the extension .bat
-    if cfg!(windows) {
-        if let Ok(rig_r) = (RCommandLine {
+    if cfg!(target_os = "windows") {
+        let rig_r_cmd = RCommandLine {
             r: Some(PathBuf::from("R.bat")),
-        })
-        .version()
-        {
+        };
+
+        if let Ok(rig_r) = rig_r_cmd.version() {
             if r_version.hazy_match(&rig_r) {
-                log::debug!("R {r_version} found on the path from `rig`");
-                return Ok(RCommandLine {
-                    r: Some(PathBuf::from("R.bat")),
-                });
+                log::debug!(
+                    "R found on the path from `rig`: {}",
+                    rig_r_cmd.clone().r.unwrap().display()
+                );
+                return Ok(rig_r_cmd);
             }
             found_r_vers.push(rig_r.original);
         }
@@ -166,14 +173,13 @@ pub fn find_r_version_command(r_version: &Version) -> Result<RCommandLine, Versi
                 .map(|p| p.path().join("bin/R"))
                 .filter(|p| p.exists())
             {
-                if let Ok(ver) = (RCommandLine {
+                let r_cmd = RCommandLine {
                     r: Some(path.clone()),
-                })
-                .version()
-                {
+                };
+                if let Ok(ver) = r_cmd.version() {
                     if r_version.hazy_match(&ver) {
-                        log::debug!(" R {r_version} found at {}", path.display());
-                        return Ok(RCommandLine { r: Some(path) });
+                        log::debug!("R found in /opt/R: {}", r_cmd.clone().r.unwrap().display());
+                        return Ok(r_cmd);
                     }
                     found_r_vers.push(ver.original);
                 }
