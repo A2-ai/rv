e4ae5d27f74442cb0082ca5475af7771bc18d0de
Wes Cummings <wes@a2-ai.com>
2026-09-23 12:17:00 -0400

    don't store res

--- .github/scripts/e2e.py
+++ .github/scripts/e2e.py
@@ -101,7 +101,7 @@ def run_test():
                 exit(1)        
          
         edit_r_version(CONFIG_FILE, "4.3")
-        res = run_rv_cmd("add", ["pkg", "--no-sync"])
+        run_rv_cmd("add", ["pkg", "--no-sync"])
         check_r_profile(False)
 
     finally:
